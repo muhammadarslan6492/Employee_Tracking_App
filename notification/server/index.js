ab686d32b2bdca0d085508e6181eabfda8533009
muhammad arslan <muhammadarslan6492@gmail.com>
2023-06-17 12:26:54 -0700

    setup notification server and configure rabbitmq and socket.io

---
+++ notification/server/index.js
@@ -0,0 +1,40 @@
+import Express from 'express';
+import * as path from 'path';
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+import * as http from 'http';
+
+const root = path.normalize(`${__dirname}/../..`);
+const app = new Express();
+const server = http.createServer(app);
+
+app.set('appPath', `${root}client`);
+app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '1000kb' }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: process.env.REQUEST_LIMIT || '1000kb',
+  }),
+);
+app.use(Express.static(path.join(__dirname, './public')));
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+try {
+  app.use((req, res) => {
+    res.status(404).json({ message: `${req.url} path not found` });
+  });
+
+  server.listen(8000, () => {
+    console.log(`app running on system ${8000}`);
+  });
+} catch (err) {
+  console.log(err);
+}
